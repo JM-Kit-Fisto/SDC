d70d78eaa5507acb50b205fcda1f6b8332458b75
Brian <itscrousie@gmail.com>
2022-09-18 12:23:01 -0400

    Match reviews schemas to csvs

--- schema.sql
+++ schema.sql
@@ -5,52 +5,58 @@ CREATE TABLE products (
 
 CREATE TABLE reviews (
   review_id int PRIMARY KEY,
-  rating int,
-  summary varchar(60),
-  recommend boolean,
-  response varchar(1000),
-  body varchar(1000),
-  review_date date,
-  reviewer_name varchar(60),
-  helpfulness int,
-  product_id int,
-  CONSTRAINT fk_products_review_id
+  product_id int
+  CONSTRAINT fk_product_id
     FOREIGN KEY(product_id)
-      REFERENCES products(product_id)
+      REFERENCES products(product_id),
+  rating int,
+  review_date int,
+  summary varchar(60),
+  body varchar(1000),
+  recommend boolean,
+  reported boolean,
+  reviewer_name varchar(60),
+  review_email varchar(60),
+  response varchar(1000),
+  helpfulness int
 );
 
 CREATE TABLE photos (
   photo_id int PRIMARY KEY,
-  photo_url varchar(255),
-  review_id int,
+  review_id int
   CONSTRAINT fk_photo_review_id
     FOREIGN KEY(review_id)
-      REFERENCES products(review_id)
+      REFERENCES reviews(review_id),
+  photo_url varchar(255),
 );
 
-CREATE TABLE metaData (
-  product_id int PRIMARY KEY,
-  ratings
-  recommend
-  characteristics
-);
+-- CREATE TABLE metaData (
+--   product_id int PRIMARY KEY,
+--   ratings
+--   recommend
+--   characteristics
+-- );
 
-CREATE TABLE ratings (
-  1_star int,
-  2_star int,
-  3_star int,
-  4_star int,
-  5_star int
-);
+-- CREATE TABLE ratings (
+--   1_star int,
+--   2_star int,
+--   3_star int,
+--   4_star int,
+--   5_star int
+--   product_id int,
+--   CONSTRAINT fk_product_id_ratings
+--     FOREIGN KEY(product_id)
+--       REFERENCES products(product_id)
+-- );
 
-CREATE TABLE characteristics (
-  char_id int PRIMARY KEY,
-  char_name varchar(60),
-  char_value
-);
+-- CREATE TABLE characteristics (
+--   char_id int PRIMARY KEY,
+--   char_name varchar(60),
 
-CREATE TABLE recommend (
-  review_id PRIMARY KEY
-  true int,
-  false int
-)
+-- );
+
+-- CREATE TABLE recommend (
+--   review_id PRIMARY KEY
+--   true int,
+--   false int
+-- )
